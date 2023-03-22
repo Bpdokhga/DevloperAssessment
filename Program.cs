99df2247f3655864484ba292a0b4fb2f529ba7c9
Brandon Dokhgan <brandondokhgan@gmail.com>
2023-03-22 17:39:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using DeveloperAssessment.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+//Logger<ContactController> controllerLogger = new Logger();
+
+// Add services to the container.
+//ContactController myContactController = new ContactController(controllerLogger);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
